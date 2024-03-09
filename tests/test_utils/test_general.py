041d6d3bbba3188dde3d9e4a8507e51a59a85f5f
libertininick <libertini.nick@gmail.com>
2024-03-09 14:57:10 -0700

    Add tests for general utilities

---
+++ tests/test_utils/test_general.py
@@ -0,0 +1,30 @@
+"""Tests for general utilities."""
+
+import time
+from pathlib import Path
+
+import pytest
+from pytest_check import check
+
+from ai_rucksack.utils.general import get_parent_directory_path, measure_execution_time
+
+
+def test_get_parent_directory_path() -> None:
+    """Test get_parent_directory_path."""
+    with check:
+        # Check that the root test directory can be found from the current file's path
+        tests_dir = get_parent_directory_path(Path(__file__), "tests")
+        assert tests_dir.name == "tests"
+        assert tests_dir.parent.name == "ai-rucksack"
+    with check:
+        # Check that a non-existent directory returns None
+        assert get_parent_directory_path(Path(__file__), "a-path-to-nowhere") is None
+
+
+def test_measure_execution_time() -> None:
+    """Test that measure_execution_time returns (roughly) the correct time."""
+    with measure_execution_time() as get_time:
+        # Sleep for 1 second
+        time.sleep(1.0)
+
+    assert pytest.approx(get_time(), 0.1) == 1.0
